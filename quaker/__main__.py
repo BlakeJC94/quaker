0fab71ea4e24158e4e7d45031a469810ec5313c9
BlakeJC94 <blakejamescook@gmail.com>
2022-10-01 15:50:21 +1000

    feat: functools partial

--- quaker/__main__.py
+++ quaker/__main__.py
@@ -1,15 +1,24 @@
+from functools import partial
+
 from fire import Fire
+
 from . import (
     __version__,
     download,
 )
+
+download_cli = partial(download, query=None)
+download_cli.__doc__ = download.__doc__
+# TODO improve CLI docs
+# TODO add CLI to quickstart
+# TODO make output_file a kwarg and download to a default location (filename with time of req)
 
 
 def main():
     Fire(
         dict(
             version=str(__version__),
-            download=download,
+            download=partial(download, query=None),
         )
     )
 
