9dadea523dbd8d4775c3d194ba87c1a6248b4c1b
BlakeJC94 <blakejamescook@gmail.com>
2022-10-01 15:28:06 +1000

    feat: added fire to requirements

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "requests",
+        "fire",
     ],
     # entry_points={
     #     'console_scripts': ['quaker=quaker.__main__:main'],
