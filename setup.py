832f453578b427ea7912f924d404582cf0553295
BlakeJC94 <blakejamescook@gmail.com>
2022-10-01 14:14:27 +1000

    chore: typo and update version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", 'r', encoding='utf-8') as f:
 
 setup(
     name="quaker",
-    version='0.2.0',
+    version='0.3.0',
     description="Lightweight python API to USGS earthquake dataset",
     long_description=long_description,
     author='BlakeJC94',
     author_email='blakejamescook@gmail.com',
-    url='https://github.com/BlakeJC94/sudoku-py',
+    url='https://github.com/BlakeJC94/quaker',
     python_requires=">=3.9",
     packages=find_packages(),
     install_requires=[
