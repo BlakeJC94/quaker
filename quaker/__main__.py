91431c804a8e3457da7a9fbd062487f7e1a494d1
BlakeJC94 <blakejamescook@gmail.com>
2022-10-11 08:08:15 +1100

    refactor: added method to create help docs from query

--- quaker/__main__.py
+++ quaker/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from typing import Tuple, Dict
 
 from . import (
     __version__,
@@ -10,24 +11,51 @@ from . import (
 logger = logging.getLogger(__name__)
 
 
-def main():
+def get_query_docs() -> Tuple[str, Dict[str, Tuple[str, str, callable]]]:
+    doc = Query.__doc__
+    annotations = Query.__annotations__
 
-    # TODO move this into a Query method
-    docs = Query.__doc__
-    head, arg_doc = docs.split("Args:")
-    arg_doc = arg_doc.replace("\n" + 12 * " ", " ")
-    extra_info = "\n".join(head.splitlines()[1:])
+    _head_doc, _arg_doc = doc.split("Args:")
+    _arg_doc = _arg_doc.replace("\n" + 12 * " ", " ")
+    query_info = "\n".join(_head_doc.splitlines()[1:])
 
-    arg_doc_dict = {}
-    for entry in arg_doc.split("\n" + 4 * " "):
+    args_info = {}
+    for entry in _arg_doc.split("\n" + 4 * " "):
         if ":" not in entry:
             continue
         name, desc = entry.split(":", 1)
-        arg_doc_dict[name.strip()] = desc.strip().lower()
+        name = name.strip()
+        desc = desc.strip().lower()
 
-    main_doc = "Access USGS Earthquake dataset" + extra_info
+        # Get the first type in square brackets, requires Optional[..]!
+        v = annotations[name]
+        type_str = str(v).replace(",", "[").split("[")[1][:-1].strip()
+        type_clb = str
+        if type_str in ["str", "float", "int"]:
+            type_clb = eval(type_str)  # pylint: disable=eval-used
+
+        metavar = "VAL"
+        name_no_3_char_prefix, name_no_5_char_prefix = name[3:], name[5:]
+        if "latitude" in [name, name_no_3_char_prefix]:
+            metavar = "LAT"
+        elif "longitude" in [name, name_no_3_char_prefix]:
+            metavar = "LNG"
+        elif "time" in [name, name_no_3_char_prefix, name_no_5_char_prefix]:
+            metavar = "TIME"
+        elif "radiuskm" in [name, name_no_3_char_prefix]:
+            metavar = "DIST"
+
+        args_info[name] = (desc, metavar, type_clb)
+
+    return query_info, args_info
+
+
+def main():
+    """Entrypoint for CLI script"""
+
+    query_info, args_info = get_query_docs()
     parser = argparse.ArgumentParser(
-        description=main_doc,
+        description="Access USGS Earthquake dataset" + query_info,
         add_help=True,
     )
 
@@ -41,40 +69,21 @@ def main():
         help=f"action to perform (default: {default_mode})",
     )
 
-    query_annotations = Query.__annotations__
-    for k, v in query_annotations.items():
-
-        # Get the first type in square brackets, requires Optional[..]!
-        type_str = str(v).replace(",", "[").split("[")[1][:-1].strip()
-        type_clb = str
-        if type_clb in ["str", "float", "int"]:
-            type_clb = eval(type_str)  # pylint: disable=eval-used
-
-        metavar = "VAL"
-        k_no_3_char_prefix, k_no_5_char_prefix = k[3:], k[5:]
-        if "latitude" in [k, k_no_3_char_prefix]:
-            metavar = "LAT"
-        if "longitude" in [k, k_no_3_char_prefix]:
-            metavar = "LNG"
-        if "time" in [k, k_no_3_char_prefix, k_no_5_char_prefix]:
-            metavar = "TIME"
-        if "radiuskm" in [k, k_no_3_char_prefix]:
-            metavar = "DIST"
-
+    for name, (desc, metavar, type_clb) in args_info.items():
         parser.add_argument(
-            "--" + k,
-            help=arg_doc_dict[k],
+            "--" + name,
+            help=desc,
             metavar=metavar,
             type=type_clb,
             required=False,
             default=None,
         )
 
-    args = parser.parse_args()
+    input_args = parser.parse_args()
 
-    fields = {k: v for k, v in vars(args).items() if k in query_annotations}
+    fields = {k: v for k, v in vars(input_args).items() if k in args_info}
     query = Query(**fields)
-    if args.mode == "download":
+    if input_args.mode == "download":
         download(output_file="/dev/stdout", query=query)
     else:
         logger.error("Only 'download' mode is supported for now")
