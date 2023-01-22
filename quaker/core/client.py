9c21ea0dcf2c09c746d4fe6f8088dc836f3be42b
BlakeJC94 <blakejamescook@gmail.com>
2023-01-23 07:33:29 +1100

    feat: added client

---
+++ quaker/core/client.py
@@ -0,0 +1,62 @@
+import logging
+from os import path, makedirs
+from time import sleep
+from typing import Union
+from requests.sessions import Session
+from quaker.core.query import Query
+from quaker.core.run import run_query
+from quaker.globals import BASE_URL, MAX_ATTEMPTS, RESPONSE_NOT_FOUND
+
+logger = logging.getLogger(__name__)
+
+
+class Client:
+    def __init__(self, output_file: str):
+        self._validate_output_file(output_file)
+        self.output_file = output_file
+
+        self.session = Session()
+
+    @staticmethod
+    def _validate_output_file(output_file: str):
+        if output_file == "/dev/stdout":
+            return
+
+        output_file = path.abspath(output_file)
+        if path.exists(output_file):
+            raise FileExistsError("File exists, remove the file or select a different destination.")
+
+        parent_dir, _ = path.split(output_file)
+        if not path.exists(parent_dir):
+            logger.info(f"Directory {parent_dir} doesnt exist, creating.")
+            makedirs(parent_dir, exist_ok=True)
+
+    def execute_paginated(self, query: Query):
+        error_recived = None
+        try:
+            # TODO Make run_query a client method
+            run_query(query, self.session, self.output_file)
+        except KeyboardInterrupt:
+            logger.error("Keyboard interrupt recieved, safely closing session.")
+        except Exception as error:  # pylint: disable=broad-except
+            logger.error(f"Unknown error recieved ({repr(error)}), safely closing session.")
+            error_recived = error
+
+        if error_recived is not None:
+            raise error_recived
+
+    def execute(self, query: Query):
+        out = None
+        with self.session as session:
+            for attempts in range(MAX_ATTEMPTS):
+                download = session.get(
+                    BASE_URL,
+                    params={**query.dict()},
+                )
+                if download.status_code != RESPONSE_NOT_FOUND:
+                    return download
+                logger.warning(f"No connection could be made, retrying ({attempts}).")
+                sleep(2)
+
+            logger.error("No connection could be made.")
+        return out
