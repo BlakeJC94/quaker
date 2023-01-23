c0fed13e31d49d5d4d5db986b5d37a13727d403d
BlakeJC94 <blakejamescook@gmail.com>
2023-01-24 08:17:50 +1100

    dev: plan execute methods

--- quaker/core/client.py
+++ quaker/core/client.py
@@ -5,7 +5,7 @@ from typing import Optional, Union
 from requests.sessions import Session
 from quaker.core.query import Query
 from quaker.core.run import run_query
-from quaker.globals import BASE_URL, MAX_ATTEMPTS, RESPONSE_NOT_FOUND
+from quaker.globals import BASE_URL, MAX_ATTEMPTS, RESPONSE_NOT_FOUND, UPPER_LIMIT
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +45,37 @@ class Client:
         if error_recived is not None:
             raise error_recived
 
-    def lookup(self, query: Query):
+    def _execute_paginiated(self, query: Query, output_file: PathLike):
+        has_next_page = True
+
+        # TODO Keep track of number of records in loop
+        # TODO implement __len__ method for result
+        if query.limit is not None:
+            raise UserWarning("query.limit not supported yet")
+            # self._execute()
+
+        # TODO Add history attr for client
+
+        _index = 0
+        while has_next_page:
+            if _index > 5:
+                raise RecursionError()
+            # Set limit
+            # Exec query
+            # Break if empty
+            # Write results
+            # If output doesn't exist, write header
+            # Get order/last field
+            # Update query
+            # Goto start
+
+        # Check if whole query was empty
+        # Write footer of last result
+
+            ...
+        ...
+
+    def _execute(self, query: Query):  # Based on get_data
         out = None
         with self.session as session:
             for attempts in range(MAX_ATTEMPTS):
